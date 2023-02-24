459c5569b696717f97df31f309a81764062a7d14
unknown <oakhmadjonov1@gmail.com>
2023-02-24 14:01:34 +0500

    started

---
+++ src/components/input/Input.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <React.Fragment>
+      <input type="text" name="name" value="value" />
+    </React.Fragment>
+  );
+};
+
+export default Input;
