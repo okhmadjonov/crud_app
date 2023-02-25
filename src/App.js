968e27eb83c27dad4bf92ea730d3c9d1a60bae4a
unknown <oakhmadjonov1@gmail.com>
2023-02-25 10:36:00 +0500

    some disegn added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import AddUser from "./pages/add-user/AddUser";
+import UsersList from "./pages/users-list/UsersList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UsersList />} />
+          <Route path="/add-user" element={<AddUser />} />
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
